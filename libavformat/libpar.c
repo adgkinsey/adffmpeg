8eeedccd82e34e08ab395ae193581725a123ea97
Gavin Kinsey <gkinsey@ad-holdings.co.uk>
2010-11-05 11:37:22 +0000

    Fixes to allow ParCopy to report scanning/conversion progress better. Change back to % for millisecond overflow check as that seems to cause less video stutters.
    git-svn-id: http://svn.ad-group.adh/svn/native_client/native/ffmpeg/trunk@4994 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- libavformat/libpar.c
+++ libavformat/libpar.c
@@ -219,6 +219,7 @@ AVStream* createStream(AVFormatContext * avf,
     char textbuffer[128];
     int w, h;
     int streamId = -1;
+    int fc = 0;
     AVStream * st = NULL;
 
     if ( (NULL == avf) || (NULL == frameInfo) || (NULL == frameInfo->frameBuffer) )
@@ -227,8 +228,9 @@ AVStream* createStream(AVFormatContext * avf,
     streamId = frameInfo->channel;
     st = av_new_stream(avf, streamId);
 
+    parReader_getIndexData(frameInfo, NULL, &fc, NULL, NULL);
     st->filename = av_strdup(avf->filename);
-    st->nb_frames = 0;
+    st->nb_frames = fc;
     st->start_time = frameInfo->indexTime * 1000 + frameInfo->indexMS;
 
     if (parReader_frameIsVideo(frameInfo))  {
@@ -589,13 +591,13 @@ static int par_read_packet(AVFormatContext * avf, AVPacket * pkt)
         return AVERROR_EOF;
     }
 
+    if (fileChanged)
+        parReader_getFilename(&p->frameInfo, avf->filename, sizeof(avf->filename));
+
     if ( (siz == 0) || (NULL == p->frameInfo.frameData) )  {
         p->frameCached = 0;
         return AVERROR(EAGAIN);
     }
-
-    if (fileChanged)
-        parReader_getFilename(&p->frameInfo, avf->filename, sizeof(avf->filename));
 
     createPacket(avf, pkt, siz, fileChanged);
 
@@ -631,7 +633,7 @@ static int par_read_seek(AVFormatContext *avf, int stream,
             }
             else  {
                 p->dispSet.timestamp = target / 1000LL;
-                p->dispSet.millisecs = target & 0x03FF;
+                p->dispSet.millisecs = target % 1000;
             }
         }
 
