28e87cc8c314bd270ea0b59a7b1c26d7fabf549e
Gavin Kinsey <gkinsey@ad-holdings.co.uk>
2011-12-14 13:05:10 +0000

    Restored copyplane filter to build, got lost in 0.9 merge
    git-svn-id: http://svn.ad-group.adh/svn/native_client/native/ffmpeg/trunk@7116 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -55,6 +55,7 @@ void avfilter_register_all(void)
     REGISTER_FILTER (BLACKFRAME,  blackframe,  vf);
     REGISTER_FILTER (BOXBLUR,     boxblur,     vf);
     REGISTER_FILTER (COPY,        copy,        vf);
+    REGISTER_FILTER (COPYPLANE,   copyplane,   vf);
     REGISTER_FILTER (CROP,        crop,        vf);
     REGISTER_FILTER (CROPDETECT,  cropdetect,  vf);
     REGISTER_FILTER (DELOGO,      delogo,      vf);
