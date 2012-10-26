dc0625c5d030590a5922f60db54659216402ae77
gkinsey <gkinsey@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2012-10-26 07:17:40 +0000

    Disabled the error check for time discontinuities of more than 60s in AVI muxing
    git-svn-id: http://svn.ad-group.adh/svn/native_client/native/ffmpeg/trunk@8795 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- libavformat/avienc.c
+++ libavformat/avienc.c
@@ -530,7 +530,7 @@ static int avi_write_packet(AVFormatContext *s, AVPacket *pkt)
 
         if(pkt->dts - avist->packet_count > 60000){
             av_log(s, AV_LOG_ERROR, "Too large number of skiped frames %"PRId64"\n", pkt->dts - avist->packet_count);
-            return AVERROR(EINVAL);
+            //return AVERROR(EINVAL);
         }
 
         av_init_packet(&empty_packet);
